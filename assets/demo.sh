d0967647a77f72f5d17ebc5aabb4e9c8e3abc0f5
Jonatan García <jonatan.garcia@mobgen.com>
2016-05-01 20:39:29 +0200

    Typo fix.

--- assets/demo.sh
+++ assets/demo.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/JonatanGarciaClavo/ContactApp
-cd ContactApp
+git clone https://github.com/JonatanGarciaClavo/ContactsApp
+cd ContactsApp
 npm install
 npm run dev
 open http://localhost:3003
